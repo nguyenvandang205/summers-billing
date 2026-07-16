7a1fc30b0ad0866f64cda267a175e46cde8b8fb9
NguyenDang <nguyenvandang205@gmail.com>
2026-07-16 11:20:13 +0700

    Fix Vercel rewrite

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,13 @@
 export default async (req, res) => {
-  const { reqHandler } = await import('../dist/fuse/server/server.mjs');
-  return reqHandler(req, res);
+  console.log('URL:', req.url);
+
+  try {
+    const mod = await import('../dist/fuse/server/server.mjs');
+    console.log('Exports:', Object.keys(mod));
+
+    return mod.reqHandler(req, res);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(String(e.stack || e));
+  }
 };
